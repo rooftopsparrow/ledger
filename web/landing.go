b56b5346fc33164a89dc9bd05855ec70b40a0502
Jonathan Nicholson <jonathan.nicholson@protectwise.com>
2021-02-22 18:28:21 -0700

    wip

--- web/landing.go
+++ web/landing.go
@@ -11,6 +11,10 @@ import (
 
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
+	"msudenver.edu/ledger/db"
+	"msudenver.edu/ledger/repos"
 )
 
 // User form fields.
@@ -20,36 +24,54 @@ type UserInfo struct {
 	Password string
 }
 
+var repo *repos.Repo
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	database := db.Init()
+	repo = repos.CreateRepo(database)
+	if err := repo.CreateSchema(database); err != nil {
+		panic(err)
+	}
+
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
-
 
 	// *** Stuck on displaying info in current page ***
 	http.HandleFunc("/welcome", registerBtn)
 
 	log.Println("Listening on port :8080...")
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	http.ListenAndServe(":8080", nil)
 }
 
-
 func registerBtn(w http.ResponseWriter, r *http.Request) {
-	// Struct fields accessible by . operator.
+
 	details := UserInfo{
 		Email:    r.FormValue("email"),
 		Name:     r.FormValue("name"),
 		Password: r.FormValue("password"),
 	}
 
-	str := "Welcome to Ledger, user: " + details.Name +
-		", email: " + details.Email
+	user, err := repo.Users.CreateUser(details.Name, details.Email)
+	if err != nil {
+		panic(err)
+	}
 
+	str := fmt.Sprintf(
+		"Welcome to Ledger! user: %s, email: %s, ID: %d, Registered on: %s",
+		user.FullName,
+		user.Email,
+		user.ID,
+		user.CreatedAt,
+	)
 	// Display user entered name & email in browser.
 	fmt.Fprintf(w, str)
 
