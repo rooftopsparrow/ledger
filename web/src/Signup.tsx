5f7157e206e80b2b97e43639a4de3dafdbc21cc0
Jonathan Nicholson <jonathan.nicholson@grey-raven.local>
2021-03-08 20:39:12 -0700

    Styling

--- web/src/Signup.tsx
+++ web/src/Signup.tsx
@@ -28,62 +28,61 @@ export async function signUp (user: UserForm): Promise<NewUser> {
   return data
 }
 
-
 export default function Signup () {
+  // Form values
   const [fullName, setFullName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirm, setConfirm] = useState('')
   const [isSubmitting, setIsSubmitting] = useState(false)
-  const [error, setError] = useState<SignupError>(null)
+  const [signUpError, setSignUpError] = useState<SignupError>(null)
+  // submit handler 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
     setIsSubmitting(true)
+    // const form = event.currentTarget
     try {
       const result = await signUp({ fullName, email, password, confirm })
       // TODO: do something
       return result
     } catch (error) {
-      setError(error)
+      setSignUpError(error)
     } finally {
       setIsSubmitting(false)
     }
   }
+  // 
   return (
-    <section id="signup">
+    <section id="create-account">
       <header>
-        <h1>Sign Up</h1>
+        <h1>Ledger</h1>
         <p>
           Integrate envelope budgeting into your existing bank.
         </p>
       </header>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
-          <input type="text" name="fullName" value={fullName} autoCorrect="off" autoComplete="off" autoCapitalize="off" onChange={e => setFullName(e.target.value)} required />
-        </label>
-        <label>
-          Email
-          <input type="email" name="email" autoComplete="email" value={email} onChange={e => setEmail(e.target.value)} required />
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} required />
-        </label>
-        <label>
-          Confirm Password
-          <input type="password" name="confirm" value={confirm} onChange={e => setConfirm(e.target.value) } required />
-        </label>
-        <legend>
-          {
-            error
-            ? <p>{error.message}</p>
-            : null
-          }
-        </legend>
+      <form id="signup" onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Sign Up for an Account</legend>
+          <label htmlFor="fullName">Name</label>
+          <input type="text" id="fullName" name="fullName" value={fullName} autoCorrect="off" autoComplete="off" autoCapitalize="off" onChange={ e => setFullName(e.target.value) } required />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" autoComplete="email" autoCorrect="off" value={email} onChange={ e => setEmail(e.target.value)} required />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" value={password} onChange={ e => setPassword(e.target.value) } required />
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" id="confirm" name="confirm" value={confirm} onChange={ e => setConfirm(e.target.value) } required />
+        </fieldset>
         <button type="submit" disabled={isSubmitting}>
           { isSubmitting ? 'Signing up...' : 'Create Account' }
         </button>
+        {
+            signUpError
+          ? 
+            <label form="signup">
+              <em>{signUpError.message}</em>
+            </label>
+          : null
+        }
       </form>
     </section>
   )
